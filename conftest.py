aa2b811e3e90cdbee4cd03306f665f64f388cf01
Leonard Crestez <cdleonard@gmail.com>
2022-10-18 01:26:25 +0300

    Fix percent decoding for caps
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- conftest.py
+++ conftest.py
@@ -1,9 +1,16 @@
+import pytest
+
 import jsonurl_py as jsonurl
 
 
 def assert_load(data, text, **kw):
     data_load = jsonurl.loads(text, **kw)
     assert data_load == data
+
+
+def assert_load_fail(text, **kw):
+    with pytest.raises(jsonurl.ParseError):
+        jsonurl.loads(text, **kw)
 
 
 def assert_roundtrip(text, data, **kw):
