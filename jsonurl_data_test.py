9aeb5706834515e6a5dbac9a428baa38f328b63a
Leonard Crestez <cdleonard@gmail.com>
2022-10-19 18:38:16 +0300

    jsonurl_data_test.py: Switch to new format
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- jsonurl_data_test.py
+++ jsonurl_data_test.py
@@ -16,15 +16,23 @@ with data_path.open("r") as f:
     JSON_TEST_DATA = json.load(f)
 
 
-@pytest.mark.parametrize("index", range(len(JSON_TEST_DATA)))
-def test(index: int):
+def _param_values():
+    return [name for name in JSON_TEST_DATA if name != "$schema"]
+
+
+@pytest.mark.parametrize("name", _param_values())
+def test(name: str):
     global JSON_TEST_DATA
-    item = JSON_TEST_DATA[index]
+    item = JSON_TEST_DATA[name]
     type = item.get("type", "roundtrip")
     if type == "roundtrip":
         text = item["text"]
         data = item["data"]
         assert jsonurl.loads(text) == data and jsonurl.dumps(data) == text
+    elif type == "load":
+        text = item["text"]
+        data = item["data"]
+        assert jsonurl.loads(text) == data
     elif type == "fail":
         text = item["text"]
         with pytest.raises(jsonurl.ParseError):
